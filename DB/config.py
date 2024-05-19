cfa678691e1bf847d1659fc3944c58962db691e2
Phisict <adrianfathan69@gmail.com>
2024-05-20 02:45:03 +0700

    db

--- DB/config.py
+++ DB/config.py
@@ -13,6 +13,6 @@ def config(filename='database.ini', section='postgresql'):
         for param in params:
             db[param[0]] = param[1]
     else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+        raise Exception('Section %s tidak ditemukan di file %s'.format(section, filename))
 
     return db
