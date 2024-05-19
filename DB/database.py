14caba88440e5060f428bf928c651fc7ca47ccec
Phisict <adrianfathan69@gmail.com>
2024-05-20 02:27:10 +0700

    src

---
+++ DB/database.py
@@ -0,0 +1,56 @@
+import ssl
+import psycopg2
+from .config import config
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+config_file = os.path.join(current_dir, "database.ini")
+config = config(config_file)
+
+class Database:
+    def __init__(self, host=None, database=None, user=None, password=None, port=None):
+        self.database = database if database else config['database']
+        self.user = user if user else config['user']
+        self.password = password if password else config['password']
+        self.host = host if host else config['host']
+        self.port = port if port else config['port']
+        self.connection = None
+        self.cursor = None
+
+    def connect(self):
+        try:
+            self.connection = psycopg2.connect(
+                database=self.database,
+                user=self.user,
+                password=self.password,
+                host=self.host,
+                port=self.port
+                # sslmode='require'
+            )
+            self.cursor = self.connection.cursor()
+            self.cursor.execute("SELECT version();")
+            self.connection.commit()
+            version = self.cursor.fetchone()
+            print("Koneksi database berhasil.",version)
+        except (Exception, psycopg2.Error) as error:
+            print("Koneksi database gagal:", error)
+
+    def disconnect(self):
+        if self.connection:
+            self.cursor.close()
+            self.connection.close()
+            print("Terputus dari database.")
+
+    def execute(self, query):
+        try:
+            self.cursor.execute(query)
+            self.connection.commit()
+            print("Query berhasil!")
+        except (Exception, psycopg2.Error) as error:
+            print("Query gagal:", error)
+
+    def fetch_data(self):
+        rows = self.cursor.fetchall()
+        for row in rows:
+            print(row)
+            
--- main.py
+++ main.py
@@ -1,55 +1,7 @@
-from sys import version
-import psycopg2
-from DB.config import config
+from db.database import Database
 
-config = config("DB/database.ini")
-class Database:
-    def __init__(self, host, database, user, password, port):
-        self.database = database
-        self.user = user
-        self.password = password
-        self.host = host
-        self.port = port
-        self.connection = None
-        self.cursor = None
+db = Database()
 
-    def connect(self):
-        try:
-            self.connection = psycopg2.connect(
-                database=self.database,
-                user=self.user,
-                password=self.password,
-                host=self.host,
-                port=self.port
-            )
-            self.cursor = self.connection.cursor()
-            self.cursor.execute("SELECT version();")
-            self.connection.commit()
-            version = self.cursor.fetchone()
-            print("Connected to the database.",version)
-        except (Exception, psycopg2.Error) as error:
-            print("Error while connecting to PostgreSQL:", error)
-
-    def disconnect(self):
-        if self.connection:
-            self.cursor.close()
-            self.connection.close()
-            print("Disconnected from the database.")
-
-    def execute(self, query):
-        try:
-            self.cursor.execute(query)
-            self.connection.commit()
-            print("Query executed successfully!")
-        except (Exception, psycopg2.Error) as error:
-            print("Error executing query:", error)
-
-    def fetch_data(self):
-        rows = self.cursor.fetchall()
-        for row in rows:
-            print(row)
-            
-db = Database(**config)
 db.connect()
 db.execute("SELECT * FROM public.mata_kuliah")
 db.fetch_data()
